5766a6d4453880f93a5d84d0ebc335d9b6d840ee
skosachiov <skosachiov@gmail.com>
2026-05-15 16:48:05 +0300

    grab back

--- grab.ino
+++ grab.ino
@@ -8,7 +8,7 @@ const int resolution = 14;    // 14-bit resolution (0 to 16383)
 // SG90 Pulse widths converted to 14-bit duty cycle values
 // 50Hz period = 20ms. 16383 units = 20ms.
 const int minDuty = 750;   // 0.5ms pulse (0 degrees) -> (0.5/20)*16383
-const int maxDuty = 2500;  // 2.4ms pulse (180 degrees) -> (2.4/20)*16383
+const int maxDuty = 2750;  // 2.4ms pulse (180 degrees) -> (2.4/20)*16383
 
 
 void setupGrab() {
@@ -19,18 +19,18 @@ void taskGrab(void *pvParameters) {
   vTaskDelay(pdMS_TO_TICKS(2000));
   setupGrab();
 
-  for (int angle = 90; angle <= 180; angle += 1) {
+  for (int angle = 0; angle <= 180; angle += 1) {
     int dutyCycle = map(angle, 0, 180, minDuty, maxDuty);
     // In Core 3.0+, ledcWrite directly takes the PIN, not the channel
     ledcWrite(servoPin, dutyCycle);
-    vTaskDelay(pdMS_TO_TICKS(5));
+    vTaskDelay(pdMS_TO_TICKS(10));
   }
   
   // Sweep from 180 to 0 degrees
   for (int angle = 180; angle >= 0; angle -= 1) {
     int dutyCycle = map(angle, 0, 180, minDuty, maxDuty);
     ledcWrite(servoPin, dutyCycle);
-    vTaskDelay(pdMS_TO_TICKS(5));
+    vTaskDelay(pdMS_TO_TICKS(10));
   }
 
   
